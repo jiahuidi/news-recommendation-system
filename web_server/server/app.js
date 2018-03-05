28ae29d64614e4225ad736d13254b5ddee64c46f
jiahuidi <jiahuidii@gmail.com>
2018-03-05 14:51:33 -0500

    add news router

--- web_server/server/app.js
+++ web_server/server/app.js
@@ -3,8 +3,16 @@ const path = require('path');
 // const favicon = require('serve-favicon');
 
 const index = require('./routes/index');
+const news = require('./routes/news');
 
 const app = express();
+
+//TODO: remove this after development is done
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -14,6 +22,7 @@ app.set('view engine', 'jade');
 app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
 
 app.use('/', index);
+app.use('/news', news);
 
 // catch 404 
 app.use(function(req, res, next) {
